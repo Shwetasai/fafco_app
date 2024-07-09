72ec8397fed1a06226fa99e67d1c7089bb0731de
shwetasai <shwetasaini250796@gmail.com>
2024-07-09 11:36:55 +0530

    developed

--- part/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import PartListCreateAPIView, PartDetailAPIView, PartcsvListView
-
-urlpatterns = [
-    path('parts/', PartListCreateAPIView.as_view(), name='part-list-create'),
-    path('parts/<int:part_id>/', PartDetailAPIView.as_view(), name='part-detail'),
-    path('partcsv/', PartcsvListView.as_view(), name='partcsv-list'),
-]
